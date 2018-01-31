8a5c362171426d71f397b4b767f1eab6f76d432d
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2018-01-31 11:44:58 -0500

    adds title and author to API call and adds UserUserId

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -18,6 +18,13 @@ module.exports = function(app) {
     res.json("/profile");
   });
 
+
+  app.post("/api/change", passport.authenticate("local"), function(req, res) {
+    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+    // So we're sending the user back the route to the members page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authed
+    // res.json("/profile");
+  });
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
@@ -30,13 +37,14 @@ module.exports = function(app) {
       city: req.body.city,
       state: req.body.state
     }).then(function() {
-      res.redirect(307, "/api/login");
+      res.redirect(307, "/api/change");
     }).catch(function(err) {
       console.log(err);
       res.json(err);
       // res.status(422).json(err.errors[0].message);
     });
   });
+
 
   // Route for logging user out
   app.get("/logout", function(req, res) {
